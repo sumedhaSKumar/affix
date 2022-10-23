041d0c014ebe3cabcb56f1059abdfedd35b1c33c
sumedha <sumedhaskumaron14@gmail.com>
2022-10-23 16:21:23 +1100

    plan for task 2

--- extract.js
+++ extract.js
@@ -1,7 +1,18 @@
 // module.exports.extract = function (window)
 
-function extractNameAttribute() {
-  console.log("tbody");
-}
+// var raw_data = document.getElementsByTagName(tbody);
+// console.log("tbody1");
+// console.log(JSON.stringify(raw_data));
+// extract objects with table = cols="2"
+// Let the ‘metadata’ for a form control be a string joining the control label and name attribute of the control with a single space character.
+// extract from each element/section 1- control label, 2- name attribute
+// key = control label , value = metadata
+// name attribute: getElementsByName()
+// control label = getElementsByTagName("td")
+//clean control label --- only if value === string type
+// metadata = control label <space> name attribute
 
-extractNameAttribute();
+// {
+//   // Write your solution to Task #2 - Extract Metadata task here
+//
+// };
